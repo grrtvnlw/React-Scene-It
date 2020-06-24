03606ed14f73153ea75aa8a72380aaf08c613b73
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-24 11:35:38 -0400

    Movie search is working

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,18 +5,28 @@ export default class Search extends Component {
     super(props)
   
     this.state = {
+      movieName: '',
       movies: []
     }
   }
-  
-  componentDidMount() {
-    fetch(`https://www.omdbapi.com/?apikey=efe3c50b&s=`)
-      .then(res => res.json())
-      .then(data => {
-        this.setState({
-          movies: data,
-        })
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    let name = this.state.movieName
+    fetch(`https://www.omdbapi.com/?apikey=efe3c50b&s=${name}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      this.setState({
+        movies: data,
       })
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      movieName: e.target.value
+    })
   }
 
   render() {
@@ -25,7 +35,7 @@ export default class Search extends Component {
         <h1>Scene It</h1>
         <h4>Search for the movies you love and save them to your watchlist.</h4>
         <form onSubmit={ this.handleFormSubmit }>
-          <input type="text" id="search" name="search" value={ this.state.movies } onChange={ this.handleChange }></input>
+          <input type="text" id="search" name="search" value={ this.state.movieName } onChange={ this.handleChange }></input>
           <button type="submit">Search</button>
         </form>
       </div>
