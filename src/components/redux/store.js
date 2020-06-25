e209df9eebca328e74415f45baf219906da5408f
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-25 11:24:39 -0400

    data is now saving to localStorage

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
 import reducer from './reducer.js'
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// check or creates persisted Storage
+const persistedState = localStorage.getItem('reduxState')
+    ? JSON.parse(localStorage.getItem('reduxState')) : localStorage.setItem('reduxState', '')
+
+const store = createStore(
+    reducer, persistedState,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// subscribes persisted storage to the global redux store
+store.subscribe(() => {
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 export default store;
