c4fa0cceaa5b7036bee4b19f1db14eda722d9f6f
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-25 22:47:04 -0400

    Added build folder

---
+++ build/precache-manifest.d5cfce7b6068b621f81b9fb663657f9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9986377547196ea2a5f909a7f9aaaa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f8af462c151c69daf8e",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "04bfad719dd742dd5cac",
+    "url": "/static/css/main.2d576ccb.chunk.css"
+  },
+  {
+    "revision": "8f8af462c151c69daf8e",
+    "url": "/static/js/2.9f9a36a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9f9a36a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bfad719dd742dd5cac",
+    "url": "/static/js/main.ba7262f9.chunk.js"
+  },
+  {
+    "revision": "9ff8ec034c058ffd8754",
+    "url": "/static/js/runtime-main.7284ad0c.js"
+  }
+]);
